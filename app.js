4da55d46019ad24a26061ec5825800f342171806
TienPhuc02 <dtp2352002@gmail.com>
2023-11-25 14:22:52 +0700

    installing & set up nodemon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
+
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on PORT :${process.env.PORT} `);
+  console.log(`Server started on PORT :${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
 });
