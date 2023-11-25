fb8f6a0ff794e1f7d6a68be5eac686f012233550
TienPhuc02 <dtp2352002@gmail.com>
2023-11-25 14:40:12 +0700

    create first route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express";
-const app = express();
+import productRoutes from "./routes/products.js";
 import dotenv from "dotenv";
+const app = express();
 dotenv.config({ path: "./config/config.env" });
 
+///import all routes
+app.use("/api/v1", productRoutes);
+
 app.listen(process.env.PORT, () => {
-  console.log(`Server started on PORT :${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+  console.log(
+    `Server started on PORT :${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
 });
---
+++ routes/products.js
@@ -0,0 +1,5 @@
+import express from "express";
+import { getProducts } from "../controllers/productControllers.js";
+const router = express.Router();
+router.route("/products").get(getProducts);
+export default router;
