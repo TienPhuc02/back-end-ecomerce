bc77cf3c63f38019a7f8f905ee525d3d910c7e58
TienPhuc02 <dtp2352002@gmail.com>
2023-11-27 15:46:00 +0700

    development vs production error

--- middlewares/error.js
+++ middlewares/error.js
@@ -3,7 +3,21 @@ export default (err, req, res, next) => {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Interval Server Error",
   };
-  res.status(error.statusCode).json({
-    message: error.message,
-  });
+  console.log(process.env.NODE_ENV);
+  console.log("error.message", error.message);
+  console.log("err", err);
+  console.log("err.stack", err.stack);
+  console.log(process.env.NODE_ENV.trim() === "DEVELOPMENT");
+  if (process.env.NODE_ENV.trim() === "DEVELOPMENT") {
+    res.status(error.statusCode).json({
+      message: error.message,
+      error: err,
+      stack: err?.stack,
+    });
+  }
+  if (process.env.NODE_ENV.trim() === "PRODUCTION") {
+    res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
 };
