5a03e5aba937f14d6876ce0a0e3a23ed736ebc0c
TienPhuc02 <dtp2352002@gmail.com>
2023-11-28 10:26:45 +0700

    Validation & mongoose ID error

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,8 +1,22 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
 export default (err, req, res, next) => {
   let error = {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Interval Server Error",
   };
+
+  //handle mongo id error
+  if (err.name === "CastError") {
+    const message = `Resource not found.Invalid ${err?.path}`;
+    error = new ErrorHandler(message, 404);
+  }
+
+  //handle validetion error
+  if (err.name === "validatoError") {
+    const message = Object.values(err.errors).map((value) => value.message);
+    error = new ErrorHandler(message, 404);
+  }
   if (process.env.NODE_ENV.trim() === "DEVELOPMENT") {
     res.status(error.statusCode).json({
       message: error.message,
