276b0d783a527334b47ba3a50f9e461bca442542
TienPhuc02 <dtp2352002@gmail.com>
2023-12-11 09:50:50 +0700

    adding user in product

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -23,6 +23,7 @@ export const getAllProducts = catchAsyncErrors(async (req, res) => {
 
 //create new product -> /api/v1/products
 export const createNewProducts = catchAsyncErrors(async (req, res) => {
+  req.body.user = req.user._id;
   const newProducts = await product.create(req.body);
   res.status(200).json({
     message: "Create New  Products Success",
@@ -66,7 +67,7 @@ export const deleteProductsDetail = catchAsyncErrors(async (req, res) => {
   if (!getProductDetails) {
     return next(new ErrorHandler("Product not found", 404));
   }
-  await product.deleteOne();
+  await product.deleteOne({ _id: req.params.id });
   res.status(200).json({
     message: "deleted  Products Details Success",
   });
--- routes/products.js
+++ routes/products.js
@@ -6,13 +6,17 @@ import {
   getProductsDetail,
   updateProductsDetail,
 } from "../controllers/productControllers.js";
-import { isAuthenticated } from "../middlewares/auth.js";
+import { authorizeRoles, isAuthenticated } from "../middlewares/auth.js";
 const router = express.Router();
+router.route("/products").get(getAllProducts);
 router
-  .route("/products")
-  .get(isAuthenticated, authorizeRoles("admin"), getAllProducts);
-router.route("/admin/products").post(createNewProducts);
+  .route("/admin/products")
+  .post(isAuthenticated, authorizeRoles("admin"), createNewProducts);
 router.route("/products/:id").get(getProductsDetail);
-router.route("/admin/products/:id").put(updateProductsDetail);
-router.route("/admin/products/:id").delete(deleteProductsDetail);
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticated, authorizeRoles("admin"), updateProductsDetail);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticated, authorizeRoles("admin"), deleteProductsDetail);
 export default router;
